94773388870249612173710afd06162ffde4bb3f
Jonathan Camacho Tencio <61394631+stevenct84@users.noreply.github.com>
2021-04-23 22:24:58 -0600

    Add files via upload

---
+++ nodoTipoFlotilla.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+
+/*
+Descripcion: clase nodo PisosHotel que posee sus atributos, constructores 
+*/
+
+class nodoTipoFlotilla {
+   public:
+    nodoTipoFlotilla(int v,string x,int z)
+    {
+       codTipo = v;
+       nombre=x;
+       cantidadTipoCarros=z;
+       Hizq =NULL;
+       Hder=NULL;
+       FB=0;
+    }
+
+	int FB;
+    int codTipo;
+    string nombre;
+    int cantidadTipoCarros;
+    nodoTipoFlotilla *Hizq;
+    nodoTipoFlotilla *Hder;
+    //nodoCarro *derecha;
+       
+   
+};
+typedef nodoTipoFlotilla *pnodoTipoFlotilla;
